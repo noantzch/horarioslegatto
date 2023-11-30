5cb2a22120840afceb1dd408edfea3a7ab86ecc8
noel antezana <noantzch@gmail.com>
2023-11-30 12:23:48 -0300

    solucion hora

--- src/ts/startAndEndTime.ts
+++ src/ts/startAndEndTime.ts
@@ -1,26 +1,25 @@
 export function getEarliestStartTime(schedule: OutputEvent[]): string {
-    let earliestStartTime: Date | null = null;
-  
-    schedule.forEach((clase) => {
+  let earliestStartTime: Date | null = null;
+
+  for (const clase of schedule) {
       const startTime = new Date(`1970-01-01T${clase.startTime}`);
       if (!earliestStartTime || startTime < earliestStartTime) {
-        earliestStartTime = startTime;
+          earliestStartTime = startTime;
       }
-    });
-  
-    return earliestStartTime?.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false }) || '09:00';
   }
-  
-  export function getLatestEndTime(schedule: OutputEvent[]): string {
-    let latestEndTime: Date | null = null;
-  
-    schedule.forEach((clase) => {
+
+  return earliestStartTime?.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false }) || '09:00';
+}
+
+export function getLatestEndTime(schedule: OutputEvent[]): string {
+  let latestEndTime: Date | null = null;
+
+  for (const clase of schedule) {
       const endTime = new Date(`1970-01-01T${clase.endTime}`);
       if (!latestEndTime || endTime > latestEndTime) {
-        latestEndTime = endTime;
+          latestEndTime = endTime;
       }
-    });
-  
-    return latestEndTime?.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false }) || '21:00';
   }
-  
+
+  return latestEndTime?.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false }) || '21:00';
+}
