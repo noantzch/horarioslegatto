0f29ff4ee3e017efbc50bdcf532d972e81b06ba6
noel antezana <noantzch@gmail.com>
2023-12-04 12:12:22 -0300

    prueba endpoint 1.8

--- src/app/api/profesores/route.ts
+++ src/app/api/profesores/route.ts
@@ -23,15 +23,24 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-    try{
-        const body = await request.json()
-        const usuario: UserResource = body.data;
+    try {
+        const body = await request.json();
+        const usuario = body.data;
+
+        // Agrega un log para mostrar los datos del usuario antes de la inserción
+        console.log('Datos del usuario:', usuario);
+
         await sql`INSERT INTO profesores (nombre, apellido, admin, user_clerk)
-                    VALUES ('${usuario.username}', '${usuario.lastName}', false, '${usuario.id}');
-                    `
-        return NextResponse.json({mensaje: 'Profesor Registrado'}, {status:201})
-    }catch(error){
-        return NextResponse.json({error}, {status: 500})
+                VALUES (${usuario.username}, ${usuario.lastName}, false, ${usuario.id})`;
+
+        // Agrega un log para indicar que la inserción se realizó con éxito
+        console.log('Profesor registrado con éxito.');
+
+        return NextResponse.json({ mensaje: 'Profesor Registrado' }, { status: 201 });
+    } catch (error) {
+        // Agrega un log para mostrar información detallada sobre el error
+        console.error('Error al registrar al profesor:', error);
+
+        return NextResponse.json({ error }, { status: 500 });
     }
-    
 }
