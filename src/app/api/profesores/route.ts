99f9179e228a33e5a2c52009f3a97b8522177552
noel antezana <noantzch@gmail.com>
2023-12-04 11:50:47 -0300

    prueba endpoint

--- src/app/api/profesores/route.ts
+++ src/app/api/profesores/route.ts
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres"
 import { NextResponse } from "next/server"
+import type { UserResource } from '@clerk/types';
 
 
 export async function GET(request: Request) {
@@ -15,6 +16,20 @@ export async function GET(request: Request) {
             return NextResponse.json({resultado: 'No hay información del profesor'}, {status: 200})
         }
         return NextResponse.json({profesor}, {status: 200})
+    }catch(error){
+        return NextResponse.json({error}, {status: 500})
+    }
+    
+}
+
+export async function POST(request: Request) {
+    try{
+        const body = await request.json()
+        const usuario: UserResource = body;
+        await sql`INSERT INTO profesores (nombre, apellido, admin, user_clerk)
+                    VALUES ('${usuario.username}', '${usuario.lastName}', false, '${usuario.id}');
+                    `
+        return NextResponse.json({mensaje: 'Profesor Registrado'}, {status:201})
     }catch(error){
         return NextResponse.json({error}, {status: 500})
     }
