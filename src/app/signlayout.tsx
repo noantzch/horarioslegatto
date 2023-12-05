6662834b5c9429f5bce429330b190921f83ed281
noel antezana <noantzch@gmail.com>
2023-12-05 10:23:50 -0300

    error clerk

---
+++ src/app/signlayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signlayout = ( children: React.ReactNode ) => {
+    return (
+      <div>
+        <div>{children}</div>
+      </div>
+    );
+  };
+
+export default Signlayout
